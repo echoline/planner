a150c7300a6c47039917eb9ca0f6ec168821e618
Linux User <eli@owl.my.domain>
2020-06-15 13:41:51 -0700

    example format to markdown script

---
+++ tomd.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# example script to convert planner data to markdown format
+
+MONTHS="January February March April May June July August September October November December"
+
+echo $USER\'s planner data
+echo =====
+echo
+
+for YYYY in $HOME/.plans/[0-9][0-9][0-9][0-9]; do
+	for MM in $YYYY/[0-1][0-9]; do
+		for DD in $MM/[0-3][0-9]; do
+			if test -f $DD/index.txt; then
+				echo -n $(echo $(basename $DD) | sed "s,^0,,");
+				echo -n " ";
+				echo -n $(echo $MONTHS | cut -d' ' -f$(basename $MM));
+				echo -n " ";
+				echo $(basename $YYYY);
+				echo ----
+				cat $DD/index.txt;
+				echo; echo;
+			fi
+		done
+	done
+done
+
