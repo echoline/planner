6db4af501ebc99053a607786efaff2a804bf1491
Linux User <eli@owl.my.domain>
2020-07-28 22:48:07 -0700

    update.sh hg support

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+ishg=0
+if test -d .hg; then
+	ishg=1
+fi
+
 for Y in [1-2][0-9][0-9][0-9]; do
 	if test ! -d $Y; then
 		continue;
@@ -16,7 +21,11 @@ for Y in [1-2][0-9][0-9][0-9]; do
 			fi
 			cd $D;
 			if test -f index.txt; then
-				mv index.txt index.md;
+				if test $ishg == 0; then
+					mv index.txt index.md;
+				else
+					hg rename index.txt index.md || mv index.txt index.md;
+				fi
 			fi
 			cd ..;
 		done
