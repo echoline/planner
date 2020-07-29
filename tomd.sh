5d8b358a86bf20b53c4ce7a4b6787e31f0ecde39
glenda <glenda@9front.local>
2020-07-29 06:50:43 +0000

    more portable tomd.sh

--- tomd.sh
+++ tomd.sh
@@ -3,17 +3,29 @@
 
 MONTHS="January February March April May June July August September October November December"
 
-echo $USER\'s planner data
+u=$user
+if test -z $u; then
+	u=$USER;
+fi
+
+u=$( echo $( echo $u | awk '{ print substr($0, 1, 1) }' | tr '[a-z]' '[A-Z]' )$( echo $u | awk '{ print substr($0, 2, length($0)-1) }' ) )
+
+echo $u\'s planner data
 echo =====
 echo
 
-for YYYY in $HOME/.plans/[0-9][0-9][0-9][0-9]; do
+DIR=$1
+if test -z $DIR; then
+	DIR=.
+fi
+
+for YYYY in $DIR/[1-2][0-9][0-9][0-9]; do
 	for MM in $YYYY/[0-1][0-9]; do
 		for DD in $MM/[0-3][0-9]; do
 			if test -f $DD/index.md; then
 				echo -n $(echo $(basename $DD) | sed "s,^0,,");
 				echo -n " ";
-				echo -n $(echo $MONTHS | cut -d' ' -f$(basename $MM));
+				echo -n $(echo $MONTHS | awk "{ print $"$(basename $MM)" }");
 				echo -n " ";
 				echo $(basename $YYYY);
 				echo ----
